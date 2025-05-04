1fc0a9cd7728e1a09f71418ccf0f85488e1f2dc4
marinna.dolado <marinna.dolado@estudiante.uam.es>
2025-05-04 14:52:49 +0200

    rpc3

--- P1-rpc-client/votoAppRPCClient/admin.py
+++ P1-rpc-client/votoAppRPCClient/admin.py
@@ -1,13 +1,3 @@
-# admin.py
 from django.contrib import admin
-from .models import Censo, Voto
 
-# Register Censo model
-@admin.register(Censo)
-class CensoAdmin(admin.ModelAdmin):
-    pass
-
-# Register Voto model
-@admin.register(Voto)
-class VotoAdmin(admin.ModelAdmin):
-    pass
+# Register your models here.
