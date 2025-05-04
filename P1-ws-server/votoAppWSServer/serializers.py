537eca1eba596bdb94816cda6c02ed2cc59b5580
marinna.dolado <marinna.dolado@estudiante.uam.es>
2025-05-04 12:01:53 +0200

    apiserver3

--- P1-ws-server/votoAppWSServer/serializers.py
+++ P1-ws-server/votoAppWSServer/serializers.py
@@ -1,45 +1,20 @@
 from rest_framework import serializers
-from . models import Voto, Censo, CodigoRespuesta
-
-class VotoSerializer(serializers.ModelSerializer):
-    """Serializador para el modelo Voto"""
-    class Meta:
-        model = Voto
-        fields = '__all__'
-        read_only_fields = ['marcaTiempo', 'codigoRespuesta']
-        extra_kwargs = {
-            'idCircunscripcion': {'required': True},
-            'idMesaElectoral': {'required': True},
-            'idProcesoElectoral': {'required': True},
-            'nombreCandidatoVotado': {'required': True},
-            'censo': {'required': True}
-        }
-        
-    def create(self, validated_data):
-        """Sobrescribimos el método create para manejar la creación de votos"""
-        # Aquí puedes agregar lógica adicional si es necesario
-        return super().create(validated_data)
+from .models import Censo, Voto
 
 class CensoSerializer(serializers.ModelSerializer):
-    """Serializador para el modelo Censo"""
     class Meta:
         model = Censo
-        fields = '__all__'
-        read_only_fields = ['numeroDNI']
-        extra_kwargs = {
-            'nombre': {'required': True},
-            'fechaNacimiento': {'required': True},
-            'anioCenso': {'required': True},
-            'codigoAutorizacion': {'required': True}
-        }
+        fields = ['numeroDNI', 'nombre', 'fechaNacimiento', 'anioCenso', 'codigoAutorizacion']
 
-class CodigoRespuestaSerializer(serializers.ModelSerializer):
-    """Serializador para el modelo CodigoRespuesta"""
+    def create(self, validated_data):
+        return validated_data
+
+    
+
+class VotoSerializer(serializers.ModelSerializer):
+    censo = serializers.CharField(source='censo.numeroDNI')
+    codigoRespuesta = serializers.CharField()
+
     class Meta:
-        model = CodigoRespuesta
-        fields = '__all__'
-        read_only_fields = ['codigoRespuesta']
-        extra_kwargs = {
-            'codigoRespuesta': {'required': True}
-        }
-
+        model = Voto
+        fields = ['id','idCircunscripcion', 'idMesaElectoral', 'idProcesoElectoral', 'nombreCandidatoVotado', 'censo', 'codigoRespuesta']
